cb3e1fdc66487768c20544ddc24235eaec1652db
Im WonYong <ssassb123@daum.net>
2022-10-26 02:45:12 +0900

    [BE] fix : User entity
    - USERS로 테이블 생성
    - Id, Column 설정
    - UserStatus 추가

--- server/pre-project/src/main/java/com/codestates/preproject/user/entity/User.java
+++ server/pre-project/src/main/java/com/codestates/preproject/user/entity/User.java
@@ -1,28 +1,55 @@
 package com.codestates.preproject.user.entity;
 
+import com.codestates.preproject.audit.BaseTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.Auditable;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.security.Principal;
 
 @NoArgsConstructor
 @Getter
 @Setter
-public class User {
+@Entity(name = "USERS")
+@Table(name = "USERS")
+public class User extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userId;
+
+    @Column(nullable = false, updatable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
     private String nickname;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private UserStatus userStatus = UserStatus.USER_ACTIVE;
 
     @Builder
     public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
+    }
+
+    public enum UserStatus {
+        USER_ACTIVE("활동중"),
+        USER_SLEEP("휴면 상태"),
+        USER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        UserStatus(String status) {
+            this.status = status;
+        }
     }
 }
