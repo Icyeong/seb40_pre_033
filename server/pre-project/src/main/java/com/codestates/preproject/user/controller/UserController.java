a2d81b9ffd0a6239a004ff27880dddb67e5fffa3
Im WonYong <ssassb123@daum.net>
2022-10-25 14:23:51 +0900

    [BE] add : UserController
    회원가입과 로그인을 위한 UserController과 UserPostDto 추가

---
+++ server/pre-project/src/main/java/com/codestates/preproject/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.codestates.preproject.user.controller;
+
+import com.codestates.preproject.user.dto.UserPostDto;
+import com.codestates.preproject.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@Validated
+@Slf4j
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/auth/signup")
+    public ResponseEntity signUp(@Valid @RequestBody UserPostDto request) {
+        return new ResponseEntity("signup", HttpStatus.CREATED);
+    }
+}
