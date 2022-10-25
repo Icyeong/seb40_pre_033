d6a482afa402d11f64fd075f41452b53d9fa09cd
devjindev <hyjinny00@gmail.com>
2022-10-25 19:12:20 +0900

    fix: App.js Router 설정2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { GlobalStyle } from './assets/styles/GlobalStyle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
 import { SignupPage } from './pages/SignupPage';
