5adbcc692a7b7935534c7badb9d50f4c375d32b2
Im WonYong <ssassb123@daum.net>
2022-10-26 14:13:39 +0900

    [BE] fix : User Service
    - 회원가입 시 email 중복 검사 실행 (email 존재할 시 409 exception 발생)
    - Password plaintext로 저장하지 않고 해싱해서 저장

---
+++ server/pre-project/src/main/java/com/codestates/preproject/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.codestates.preproject.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
