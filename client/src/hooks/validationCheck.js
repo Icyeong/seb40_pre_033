61d7e2496edad8ab412a2a69d9189fa7cfd01d8c
Icyeong <dbcjswjs@naver.com>
2022-10-28 00:46:59 +1100

    refactor: 유효성 검사기능 분리/ 향후 수정 필요

---
+++ client/src/hooks/validationCheck.js
@@ -0,0 +1,19 @@
+const validationCheck = async () => {
+  //     const [email, emailFormEl, password, passwordFormEl, isValid, ] = props;
+  //   console.log(props);
+
+  //   if (email.trim() === '') {
+  //     emailFormEl.current.classList.add('inValid');
+  //     return setInvalidEmail('Email cannot be empty.');
+  //   } else if (password.trim() === '') {
+  //     passwordFormEl.current.classList.add('inValid');
+  //     return setInvalidPassword('Password cannot be empty.');
+  //   } else if (!isValid) {
+  //     emailFormEl.current.classList.add('inValid');
+  //     return setInvalidEmail('The email is not a valid email address.');
+  //   }
+
+  return;
+};
+
+export default validationCheck;
