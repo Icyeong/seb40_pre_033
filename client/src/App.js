58024d1e2eaae3c0737133f6ea6805a4bb80e38b
youthmn <banghmin@hanmail.net>
2022-10-27 09:58:45 +0900

    app.js 초기화

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './assets/styles/GlobalStyle';
 import './assets/styles/variables.css';
-import AskQuestions from './pages/WritePage/AskQuestions.js';
 
 function App() {
-  // const newLocal = '/write';
   return (
-    <BrowserRouter>
+    <>
       <GlobalStyle />
-      <Routes>
-        {/* <Route path="/" exact={true} component={EditPage} /> */}
-        <Route path="/" element={<AskQuestions />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <div>삼삼조 화이팅</div>
+    </>
   );
 }
 
 export default App;
-
-// * 지우지말것
-// import { GlobalStyle } from './assets/styles/GlobalStyle';
-// import './assets/styles/variables.css';
-
-// function App() {
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <div>삼삼조 화이팅</div>
-//     </>
-//   );
-// }
-
-// export default App;
