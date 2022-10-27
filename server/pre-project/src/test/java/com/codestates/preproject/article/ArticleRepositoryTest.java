b1b22db246513508de492254917c91d838191296
Tldkt <evolym2543@sookmyung.ac.kr>
2022-10-28 01:23:48 +0900

    [BE][test] Article 조회 테스트 추가 ArticleRepositoryTest findArticle_Success() ArticleFactory 클래스 작성

--- server/pre-project/src/test/java/com/codestates/preproject/article/ArticleRepositoryTest.java
+++ server/pre-project/src/test/java/com/codestates/preproject/article/ArticleRepositoryTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
 
 import  static org.assertj.core.api.Assertions.*;
 
@@ -33,4 +37,50 @@ class ArticleRepositoryTest {
         assertThat(result.getContents()).isEqualTo("질문내용");
         assertThat(result.getTitle()).isEqualTo("질문제목");
     }
+
+    @DisplayName("전체 게시글을 최신순으로 가져온다.")
+    @Test
+    void findAllArticle_Success() {
+/*        // given - mockUsers 추가 예정
+        User tester = UserFactory.user("tester");
+        List<User> mockUsers = List.of(
+                UserFactory.user("a1"),
+                UserFactory.user("a2"),
+                UserFactory.user("a3"),
+                UserFactory.user("a4"),
+                UserFactory.user("a5"),
+                UserFactory.user("a6"),
+                UserFactory.user("a7"),
+                UserFactory.user("a8")
+        );
+        userRepository.save(tester);
+        userRepository.saveAll(mockUsers);
+*/
+        // given - mockArticles
+        List<Article> mockArticles = ArticleFactory.mockArticles; //By(mockUsers); 추가 예정
+        Article testerArticle = ArticleFactory.mockArticleBy(tester);
+        ArticleRepository.saveAll(mockArticles);
+        ArticleRepository.save(testerArticle);
+
+        flushAndClear();
+
+        // when
+        List<Article> feeds =
+                ArticleRepository.findAll(tester, PageRequest.of(0, 10));
+
+        // then
+        assertThat(feeds)
+                .extracting("user")
+                .extracting("basicProfile")
+                .extracting("name")
+                .containsExactly("tester", "a7", "a5", "a3", "a1");
+    }
+
+    private void flushAndClear() {
+        testEntityManager.flush();
+        testEntityManager.clear();
+    }
+
+
+
 }
