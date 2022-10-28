71ce7c53f934a40670fc59732b026e7fc744286c
yelinz515 <ggg06141@gmail.com>
2022-10-28 10:18:49 +0900

    home 컴포넌트 분리

--- client/src/components/Home/SidebarWidget/SidebarWidget.js
+++ client/src/components/Home/SidebarWidget/SidebarWidget.js
@@ -1,42 +1,5 @@
-import styled from 'styled-components';
+import { RightSide, RightSidebar } from './SidebarWidget_style';
 import { EditWidget } from './EditWidget';
-
-export const RightSide = styled.div`
-  /* margin 사이즈 수정 가능 */
-  margin-top: 80px;
-  display: flex;
-  flex-direction: column;
-  width: 302px;
-`;
-
-export const RightSidebar = styled.div`
-  border: 1px solid #f1e5bc;
-  background-color: #fbf3d5;
-  color: #525960;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), 0 1px 2px rgba(0, 0, 0, 0.05),
-    0 2px 8px rgba(0, 0, 0, 0.05);
-
-  header {
-    padding: 12px 15px;
-    border-bottom: 1px solid #f1e5bc;
-    font-size: 13px;
-    font-weight: bold;
-  }
-
-  & > header:nth-child(3) {
-    border-top: 1px solid #f1e5bc;
-  }
-
-  ul {
-    padding: 4px 15px;
-    background-color: #faf5e6;
-  }
-  li {
-    margin: 12px 0;
-    font-size: 13px;
-    list-style: inside;
-  }
-`;
 
 export const SidebarWidget = () => {
   return (
