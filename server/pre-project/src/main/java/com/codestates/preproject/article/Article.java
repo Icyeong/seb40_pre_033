9bb75d596147e80c27cad412bf05c8b403c3ae5e
Tldkt <evolym2543@sookmyung.ac.kr>
2022-10-30 17:27:13 +0900

    [BE] fix: article 엔티티 수정
    - Article Column상세설정

--- server/pre-project/src/main/java/com/codestates/preproject/article/Article.java
+++ server/pre-project/src/main/java/com/codestates/preproject/article/Article.java
@@ -4,19 +4,28 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
+
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Article {
     // merge 이후 extends BaseTime 추가
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)//기본키 자동생성 목적
-    Long articleId;
-    @Column(length=200)
-    String title;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)//기본키 자동생성 목적
+    @Column(name="article_id")
+    private Long articleId;
+
+    @Column(nullable = false)
+    private String title;
+
     @Lob
-    String contents;
+    @Column(nullable = false)
+    private String contents;
+
+    //추후 추가 예정
+    /*@ManyToOne @JoinColumn(name = "user_id")
+    private User writer;
+    */
 
 }
