b07920af1c1c59c450fcb2ff6cf6584187e8582c
Icyeong <dbcjswjs@naver.com>
2022-10-30 14:33:20 +0900

    feat: popover 기능 구현

--- client/src/components/login-logout-signup/OptionalInput.js
+++ client/src/components/login-logout-signup/OptionalInput.js
@@ -1,6 +1,23 @@
 import { QuestionmarkSvg } from '../../assets/images/SignupSvg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Popover, OverlayTrigger, Button } from 'react-bootstrap';
 
 const OptionalInput = () => {
+  const popoverLeft = (
+    <Popover id="popover-positioned-left" title="Popover left">
+      <Popover.Body>
+        <p>
+          We know you hate spam, and we do too. That’s why we make it easy for
+          you to update your email preferences or unsubscribe at anytime.
+        </p>
+        <p>
+          We never share your email address with third parties for marketing
+          purposes.
+        </p>
+      </Popover.Body>
+    </Popover>
+  );
+
   return (
     <div className="optionalCheck">
       <input id="opt-check" type="checkbox" />
@@ -8,7 +25,12 @@ const OptionalInput = () => {
         Opt-in to receive occasional product updates, user research invitations,
         company announcements, and digests.
       </label>
-      <QuestionmarkSvg />
+
+      <OverlayTrigger trigger="click" placement="left" overlay={popoverLeft}>
+        <Button className="bootsBtn">
+          <QuestionmarkSvg />
+        </Button>
+      </OverlayTrigger>
     </div>
   );
 };
