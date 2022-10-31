0a796f0a85cd5459da56c3c82edbde180e0dad2a
2D3 <as13337@gmail.com>
2022-10-31 13:00:01 +0900

    add: @NotBlank

--- server/pre-project/src/main/java/com/codestates/preproject/comment/dto/CommentPostDto.java
+++ server/pre-project/src/main/java/com/codestates/preproject/comment/dto/CommentPostDto.java
@@ -1,14 +1,23 @@
 package com.codestates.preproject.comment.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class CommentPostDto {
 
+    @NotBlank
     private long commentId;
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String content;
 
 }
