5c878860cd63295435b4e285ef842073c9740950
Icyeong <dbcjswjs@naver.com>
2022-11-01 20:48:43 +0900

    fix: proxy설정 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/auth',
     createProxyMiddleware({
-      target: 'https://d92f-114-205-132-181.jp.ngrok.io',
+      target: 'https://bea9-14-55-61-123.jp.ngrok.io',
       changeOrigin: true,
     })
   );
