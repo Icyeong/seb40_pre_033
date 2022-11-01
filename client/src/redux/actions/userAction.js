f21467d6022f3b720c029aa00e74b888181247a2
Icyeong <dbcjswjs@naver.com>
2022-11-01 21:09:17 +0900

    chore: 리팩토링 필요

--- client/src/redux/actions/userAction.js
+++ client/src/redux/actions/userAction.js
@@ -1,12 +1,53 @@
-import useFetch from '../../hooks/useFetch';
+import axios from 'axios';
+// import useFetch from '../../hooks/useFetch';
 
 export const REGISTER_USER = 'REGISTER_USER';
 
-export const registerUser = (userData) => {
-  const res = useFetch('post', '/auth/signup', userData);
+export const registerUser = () => {
+  // const res = useFetch('post', '/auth/signup', userData);
+  // console.log(res);
+  // return {
+  //   type: REGISTER_USER,
+  //   payload: res,
+  // };
 
-  return {
-    type: REGISTER_USER,
-    payload: res,
-  };
+  // const requestOptions = {
+  //   Headers: {
+  //     'Content-Type': 'application/json',
+  //     'ngrok-skip-browser-warning': 'any value',
+  //   },
+  //   Body: JSON.stringify(userData),
+  // };
+
+  // try {
+  //   const response = fetch(`/auth/signup`, requestOptions)
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+
+  //   return {
+  //     type: REGISTER_USER,
+  //     payload: response.data,
+  //   };
+  // } catch (err) {
+  //   throw new Error('err2');
+  // }
+
+  try {
+    const res = axios.get('/users').then((res) => console.log(res));
+    // const res = axios
+    //   .post(`/auth/signup`, requestOptions)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    console.log(res);
+    return {
+      type: REGISTER_USER,
+      payload: res.data,
+    };
+  } catch (err) {
+    throw new Error('err 발생');
+  }
 };
