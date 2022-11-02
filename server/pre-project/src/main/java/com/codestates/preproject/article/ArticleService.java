266f8fce6a4a59f9c9366a4711a07a179e59837d
Tldkt <evolym2543@sookmyung.ac.kr>
2022-11-02 15:14:50 +0900

    [BE]chore : ArticleService transactional 추가

--- server/pre-project/src/main/java/com/codestates/preproject/article/ArticleService.java
+++ server/pre-project/src/main/java/com/codestates/preproject/article/ArticleService.java
@@ -1,11 +1,13 @@
 package com.codestates.preproject.article;
 
-import com.codestates.preproject.exception.*;
+import com.codestates.preproject.exception.BusinessLogicException;
+import com.codestates.preproject.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,19 +15,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
-
-
-
+    @Transactional(readOnly = true)
     public Article createArticle(Article article) {
         return articleRepository.save(article);
     }
-
+    @Transactional(readOnly = true)
     public Article findArticle(long articleId) {
         return findVerifiedArticle(articleId);
     }
 
 
-
+    @Transactional(readOnly = true)
     public Article updateArticle(Article article) {
         Article foundArticle = findVerifiedArticle(article.getArticleId());
 
@@ -33,13 +33,13 @@ public class ArticleService {
                 .ifPresent(title -> foundArticle.setTitle(title)); //To Do: setter패턴 builder로 변경
 
         Optional.ofNullable(article.getContent())
-                .ifPresent(contents -> foundArticle.setContent(contents));//To Do: setter패턴 builder로 변경
+                .ifPresent(content -> foundArticle.setContent(content));//To Do: setter패턴 builder로 변경
 
         return articleRepository.save(foundArticle);
 
     }
-
-    private Article findVerifiedArticle(long coffeeId) {
+    @Transactional(readOnly = true)
+    public Article findVerifiedArticle(long coffeeId) {
         Optional<Article> optionalArticle = articleRepository.findById(coffeeId);
         Article foundArticle =
                 optionalArticle.orElseThrow(() ->
@@ -47,16 +47,33 @@ public class ArticleService {
 
         return foundArticle;
     }
-
+    @Transactional(readOnly = true)
     public void deleteArticle(long memberId) {
         Article foundArticle = findVerifiedArticle(memberId);
 
         articleRepository.delete(foundArticle);
     }
-
+    @Transactional(readOnly = true)
     public Page<Article> findArticles(int page, int size) {
         return articleRepository.findAll(PageRequest.of(page, size, Sort.by("articleId").descending()));
     }
+
+/*    @Transactional(readOnly = true)
+    public List<CommentResponseDto> findComments(String email) {
+        User findUser = userRepository.findByEmail(email).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+
+        List<Comment> comments = commentRepository.findAllByUser_userId(findUser.getUserId());
+
+        return comments.stream()
+                .map(comment -> CommentResponseDto.builder()
+                        .commentId(comment.getCommentId())
+                        .username(comment.getUsername())
+                        .content(comment.getContent())
+                        .build())
+                .collect(Collectors.toList());
+    }*/
+
+
 
 
 
