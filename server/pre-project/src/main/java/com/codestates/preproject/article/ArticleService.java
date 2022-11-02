304ee8f530fac9bd8796df702317f1ddbac88b8c
Tldkt <evolym2543@sookmyung.ac.kr>
2022-11-02 21:44:40 +0900

    [BE]chore : json 응답 데이터 수정, 오타 수정

--- server/pre-project/src/main/java/com/codestates/preproject/article/ArticleService.java
+++ server/pre-project/src/main/java/com/codestates/preproject/article/ArticleService.java
@@ -25,7 +25,6 @@ public class ArticleService {
         User findUser = optionalUser.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
         /*에러가 뜨면 에러 던지고 아닌 경우 받아옴*/
         article.setUser(findUser);
-
         article.setEmail(email);
 
         return articleRepository.save(article);
@@ -37,7 +36,7 @@ public class ArticleService {
     }
 
 
-    @Transactional(readOnly = true)
+    @Transactional
     public Article updateArticle(Article article) {
         Article foundArticle = findVerifiedArticle(article.getArticleId());
 
@@ -70,24 +69,5 @@ public class ArticleService {
     public Page<Article> findArticles(int page, int size) {
         return articleRepository.findAll(PageRequest.of(page, size, Sort.by("articleId").descending()));
     }
-
-/*    @Transactional(readOnly = true)
-    public List<CommentResponseDto> findComments(String email) {
-        User findUser = userRepository.findByEmail(email).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
-
-        List<Comment> comments = commentRepository.findAllByUser_userId(findUser.getUserId());
-
-        return comments.stream()
-                .map(comment -> CommentResponseDto.builder()
-                        .commentId(comment.getCommentId())
-                        .username(comment.getUsername())
-                        .content(comment.getContent())
-                        .build())
-                .collect(Collectors.toList());
-    }*/
-
-
-
-
 
 }
