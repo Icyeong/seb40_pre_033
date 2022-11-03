c0f80d32b9d98a4044f75c802011d661d609b741
Icyeong <dbcjswjs@naver.com>
2022-11-03 23:04:33 +0900

    feat: token 만료 구분 및 재발급 기능 구현완료

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,47 @@ import { QuestionPage } from './pages/QuestionPage';
 import { QuestionWritePage } from './pages/QuestionWritePage';
 import { QuestionEditPage } from './pages/QuestionEditPage';
 import { AnswerEdit } from './pages/AnswerEdit';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getLoginStatus } from './redux/actions/userAction';
+import jwt_decode from 'jwt-decode';
+import { refreshToken } from './hooks/refreshToken';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      try {
+        const { exp } = jwt_decode(token);
+        // const time = Date.now();
+        if (Date.now() >= exp * 1000) {
+          console.log('토큰이 만료됬습니다.');
+          console.log(exp);
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('refreshToken');
+          dispatch(getLoginStatus({ isLogin: false }));
+          // window.reload();
+
+          // 토큰 만료 전 로그인 연장 필요
+        } else if (Date.now() >= exp * 1000 - 100000) {
+          dispatch(getLoginStatus({ isLogin: true }));
+          console.log('토큰이 재발급이 필요.');
+          refreshToken();
+          console.log(exp);
+          // 토큰 유효
+        } else {
+          dispatch(getLoginStatus({ isLogin: true }));
+          console.log('토큰이 유효합니다.');
+          console.log(exp);
+        }
+        console.log('지금시각 : ', Date.now(), 'exp : ', exp * 1000);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
   return (
     <>
       <GlobalStyle />
