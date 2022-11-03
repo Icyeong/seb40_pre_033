f66bbc9504950d69e2d6c8ea23de63e42e1cc5ae
Im WonYong <ssassb123@daum.net>
2022-11-03 13:37:51 +0900

    [BE] fix : user controller response dto 변경

--- server/pre-project/src/main/java/com/codestates/preproject/user/controller/UserController.java
+++ server/pre-project/src/main/java/com/codestates/preproject/user/controller/UserController.java
@@ -1,18 +1,12 @@
 package com.codestates.preproject.user.controller;
 
-import com.codestates.preproject.article.ArticleMapper;
-import com.codestates.preproject.article.ArticleResponse;
-import com.codestates.preproject.comment.dto.CommentResponseDto;
-import com.codestates.preproject.security.userDetails.PrincipalDetailsService;
+import com.codestates.preproject.response.SingleResponseDto;
 import com.codestates.preproject.user.dto.UserPostDto;
-import com.codestates.preproject.user.dto.UserResponseDto;
 import com.codestates.preproject.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,34 +25,34 @@ public class UserController {
     }
 
     @PostMapping("/auth/signup")
-    public ResponseEntity<UserResponseDto> signUp(@Valid @RequestBody UserPostDto request) {
-        return new ResponseEntity<>(userService.createUser(request), HttpStatus.CREATED);
+    public ResponseEntity<SingleResponseDto> signUp(@Valid @RequestBody UserPostDto request) {
+        return new ResponseEntity<>(new SingleResponseDto<>(userService.createUser(request)), HttpStatus.CREATED);
     }
 
     @GetMapping("/user/{user-id}")
-    public ResponseEntity<UserResponseDto> getUser(@Positive @PathVariable("user-id") long userId) {
-        return new ResponseEntity<>(userService.findUser(userId), HttpStatus.OK);
+    public ResponseEntity<SingleResponseDto> getUser(@Positive @PathVariable("user-id") long userId) {
+        return new ResponseEntity<>(new SingleResponseDto(userService.findUser(userId)), HttpStatus.OK);
     }
 
     @GetMapping("/user/me/articles")
-    public ResponseEntity<List<ArticleResponse>> getMyArticles() {
+    public ResponseEntity<SingleResponseDto> getMyArticles() {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        return new ResponseEntity<>(userService.getMyArticles(email), HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(userService.getMyArticles(email)), HttpStatus.OK);
     }
 
     @GetMapping("/user/me/comments")
-    public ResponseEntity<List<CommentResponseDto>> getMyComments() {
+    public ResponseEntity<SingleResponseDto> getMyComments() {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        return new ResponseEntity<>(userService.getMyComments(email), HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto(userService.getMyComments(email)), HttpStatus.OK);
     }
 
 
     @GetMapping("/user/me")
-    public ResponseEntity<UserResponseDto> getMe() {
+    public ResponseEntity<SingleResponseDto> getMe() {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        return new ResponseEntity<>(userService.getMe(email), HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto(userService.getMe(email)), HttpStatus.OK);
     }
 }
