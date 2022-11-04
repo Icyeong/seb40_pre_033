00397c2bdeb2073c0926bfdf4066194e5d1a43b3
2D3Edith <as13337@gmail.com>
2022-11-04 17:06:14 +0900

    추가: 리댓 리포지터리 추가

---
+++ server/pre-project/src/main/java/com/codestates/preproject/reply/repository/ReplyRepository.java
@@ -0,0 +1,14 @@
+package com.codestates.preproject.reply.repository;
+
+import com.codestates.preproject.reply.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    Reply findByReplyId(long replyId);
+
+//    List<NestedComment> findByNestedComments(Comment comment);
+
+}
