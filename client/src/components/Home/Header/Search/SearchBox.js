73c0ba090f78274376da47e8ea405b8c3c431257
Icyeong <dbcjswjs@naver.com>
2022-11-06 16:22:04 +0900

    fix: search modal 버그 수정

--- client/src/components/Home/Header/Search/SearchBox.js
+++ client/src/components/Home/Header/Search/SearchBox.js
@@ -4,33 +4,23 @@ import { SearchHints } from './SearchHints';
 
 export const SearchBox = () => {
   const modalRef = useRef();
+  const searchRef = useRef();
 
-  const onFocusInput = () => {
-    if (!modalRef.current) {
-      return;
+  const modalHandler = (e) => {
+    if (searchRef.current.contains(e.target)) {
+      modalRef.current.style.display = 'block';
+    } else {
+      modalRef.current.style.display = 'none';
     }
-    modalRef.current.style.display = 'block';
-    console.log(modalRef.current.style);
   };
 
-  const onBlurInput = () => {
-    if (!modalRef.current) {
-      return;
-    }
-    modalRef.current.style.display = 'none';
-  };
+  window.addEventListener('click', modalHandler);
 
   return (
-    <form className="search">
+    <form ref={searchRef} className="search">
       <div className="input-search">
         <AiOutlineSearch size={20} color="#838C95" />
-        <input
-          type="text"
-          className="logo-search"
-          placeholder="Search..."
-          onFocus={onFocusInput}
-          onBlur={onBlurInput}
-        />
+        <input type="text" className="logo-search" placeholder="Search..." />
         <div ref={modalRef} className="search-hints">
           <SearchHints />
         </div>
